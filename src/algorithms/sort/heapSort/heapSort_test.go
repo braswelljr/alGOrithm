7521558d6ed426d110410b59bc0296581663ea2c
braswelljr <braswellkenneth7@gmail.com>
2022-01-05 18:58:16 +0000

    HeapSort

---
+++ src/algorithms/sort/heapSort/heapSort_test.go
@@ -0,0 +1,14 @@
+package heapSort
+
+import "testing"
+
+func TestHeapSort(t *testing.T) {
+  arr := []int{2, 4, 3, 5, 1}
+  arr = HeapSort(arr)
+  if arr[0] != 1 || arr[1] != 2 || arr[2] != 3 || arr[3] != 4 || arr[4] != 5 {
+    t.Error("QuickSort() failed. Got", arr, "Expected 1 2 3 4 5")
+    return
+  }
+
+  t.Log("QuickSort() passed.")
+}
